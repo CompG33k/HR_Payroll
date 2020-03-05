065b414c2e0ce95865f8d773c454c0f03327aeea
CompG33k <N.J.Velasco@sbcglobal.net>
2020-03-05 10:08:39 -0800

    Last commit before submission adding store procedures/functions

---
+++ HR_Payroll_Library/SQLScripts/Functions.sql
@@ -0,0 +1,63 @@
+﻿USE [HRDemoDB]
+GO
+
+/****** Object:  UserDefinedFunction [dbo].[GetYearsEmployed]    Script Date: 3/5/2020 10:07:17 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE FUNCTION [dbo].[GetYearsEmployed](@EmployeeID int)  
+RETURNS int   
+AS   
+-- Returns the number of years employed.  
+BEGIN  
+    DECLARE @ret int;  
+    
+	SELECT @ret = YEAR(getDate()) - YEAR([StartDate]) 
+	FROM
+	[HRDemoDB].[dbo].[Employee]
+	WHERE
+	[EndDate] IS NULL
+	AND 
+	[ID] = @EmployeeID;
+
+     IF (@ret IS NULL)   
+        SET @ret = 0;  
+    RETURN @ret;  
+END; 
+
+GO
+
+
+USE [HRDemoDB]
+GO
+
+/****** Object:  UserDefinedFunction [dbo].[GetNumberOfDependents]    Script Date: 3/5/2020 10:07:10 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE FUNCTION [dbo].[GetNumberOfDependents](@EmployeeID int)  
+RETURNS int   
+AS   
+-- Returns the number of years employed.  
+BEGIN  
+    DECLARE @ret int;  
+    
+	SELECT @ret = count(*) 
+	FROM  [HRDemoDB].[dbo].[Dependent]
+	where [employeeID] = @EmployeeID;
+
+    IF (@ret IS NULL)   
+        SET @ret = 0;  
+    RETURN @ret;  
+END; 
+
+
+GO
+
+
---
+++ HR_Payroll_Library/SQLScripts/StoredProcedures.sql
@@ -0,0 +1,36 @@
+﻿USE [HRDemoDB]
+GO
+
+/****** Object:  StoredProcedure [dbo].[GetEmployeeSummary]    Script Date: 3/5/2020 10:06:03 AM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[GetEmployeeSummary]
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT [ID]
+      ,[fname]
+      ,[lname]
+      ,[StartDate]
+      ,[benefit_enrolled]
+	  ,[dbo].[GetNumberOfDependents]([ID]) as Dependents
+	  ,[dbo].[GetYearsEmployed]([ID]) as YearsEmployed
+  FROM [HRDemoDB].[dbo].[Employee]
+
+END
+GO
+
+
